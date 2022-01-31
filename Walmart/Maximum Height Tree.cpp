1d6edb29ce17b7ed9ad1137d8a37dc9566773f6c
Shashikant Solanki <90645693+hey-shashikant@users.noreply.github.com>
2022-01-31 13:01:37 +0530

    Maximum Height Tree

---
+++ Walmart/Maximum Height Tree.cpp
@@ -0,0 +1,31 @@
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int height(int n){
+        // code here
+        int x=(-1+sqrt(1+8*n))/2;
+        return x;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        cout<<ob.height(N)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
